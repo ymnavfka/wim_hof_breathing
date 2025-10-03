8232c7d7756eeed288cfc698db347c0d38bd74ce
Morozov Nikita <morozov.nikita.job@gmail.com>
2025-10-03 09:50:27 +0300

    Remove debug banner

--- wim_hof_breathing/lib/main.dart
+++ wim_hof_breathing/lib/main.dart
@@ -11,6 +11,7 @@ class WimHofApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Wim Hof Breathing',
       theme: ThemeData(primarySwatch: Colors.blue),
       home: const MainScreen(),
